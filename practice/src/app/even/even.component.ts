7ca17b1ba0f73ede7d309b0788bd30aea6627622
Rachel Vargas <angeles.vargas@ymail.com>
2023-07-26 16:07:21 -0700

    I have added game control assignment

---
+++ practice/src/app/even/even.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrls: ['./even.component.css']
+})
+export class EvenComponent {
+  @Input() number: number;
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
